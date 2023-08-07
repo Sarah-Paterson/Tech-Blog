b546610045a93dcb0cfa4267a00c47413bc7b7ad
kitkatyourmom <sarahlridings@gmail.com>
2023-08-06 23:18:44 -0600

    add post model

---
+++ models/Post.js
@@ -0,0 +1,38 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Post extends Model {}
+Post.init(
+  {
+    post_id: {
+      type: DataTypes.BIGINT,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    title: {
+      type: DataTypes.STRING(500),
+      allowNull: false,
+    },
+    content: {
+      type: DataTypes.STRING(5000),
+      allowNull: false,
+    },
+    author: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    date: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    underscored: true,
+    modelName: 'post',
+  },
+);
+
+module.exports = Post;
