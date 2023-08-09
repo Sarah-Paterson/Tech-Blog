261e6670fb146e91164aac2250d33b66fd5a9bd5
kitkatyourmom <sarahlridings@gmail.com>
2023-08-09 00:44:59 -0600

    working with routers - testing error

--- server.js
+++ server.js
@@ -1,13 +1,28 @@
 const path = require('path');
 const express = require('express');
+const session = require('express-session');
 const exphbs = require('express-handlebars');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.engine('handlebars', exphbs.engine);
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
+
+app.use(session(sess));
+
+app.engine('handlebars', exphbs);
 app.set('view engine', 'handlebars');
 
 app.use(express.json());
